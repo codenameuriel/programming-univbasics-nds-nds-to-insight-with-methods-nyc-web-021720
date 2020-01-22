3a69495af727134aa98e320e883ac7315eaa247a
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-22 01:27:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ def gross_for_director(director_data)
   total = 0 
   directors_movies = directors_data[outer_index][:movies]
   outer_index = 0 
-  director_movies.length.times |i| do
+  director_movies.length.times do |i|
     total += director_movies[i][:worldwide_grosses]
   end
   return total
